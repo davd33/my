ed118d538e82d2b8a365ee2e18478105eb0ab41f
David Rueda <davd33@gmail.com>
2018-06-30 16:14:52 +0545

    cv - page title

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-cv',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CvComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleSvc: Title) { }
 
   ngOnInit() {
+    this.titleSvc.setTitle('David Rueda - Resumé')
   }
 
 }
